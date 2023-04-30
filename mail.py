3cf754803607ad5e96016c10f616781e396270fc
Mary <66644979+Mechnuna@users.noreply.github.com>
2023-04-30 14:44:12 +0300

    Update mail.py
    Добавила класс GameSprite

--- mail.py
+++ mail.py
@@ -5,11 +5,14 @@ back = (0,0,0)
 mw = pygame.display.set_mode((600,600))
 mw.fill(back)
 
+class GameSprite:
+    pass
+
 clock = pygame.time.Clock()
 game = True
 while game:
     for e in pygame.event.get():
-        if e.type == QUIQ:
+        if e.type == QUIT:
             game = False
 
     pygame.display.update()
